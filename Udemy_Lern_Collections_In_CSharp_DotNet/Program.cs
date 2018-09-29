50ff8c6730e9a7582037a88930c0283395ab8656
Sean Newman <seangnewman@msn.com>
2018-09-29 02:26:20 -0700

    Add project files.

---
+++ Udemy_Lern_Collections_In_CSharp_DotNet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_Lern_Collections_In_CSharp_DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
